e41524a3a3064aea995eb9f2071e7cac6ac6a1ff
Larnlord <88141978+Larnlord@users.noreply.github.com>
2021-11-25 13:45:33 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 
+  <!--- footer ---->
+  <footer class="footer">
+  <div class="container content has-text-centered">
+  <p> Made with money by Mike</p> 
+  </div>
+
+  </footer>
+
+    style:
 })
 export class AppComponent {
   title = 'privot';
